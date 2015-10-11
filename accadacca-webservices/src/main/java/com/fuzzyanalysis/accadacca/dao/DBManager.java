c0348dce78ae667e44ccf394102fc46fca251f13
fuzzyanalysis <sh4ngithub>
2015-10-11 16:33:12 +1100

    fuzzy - testing git.ignore.

--- accadacca-webservices/src/main/java/com/fuzzyanalysis/accadacca/dao/DBManager.java
+++ accadacca-webservices/src/main/java/com/fuzzyanalysis/accadacca/dao/DBManager.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import com.fuzzyanaylsis.accadacca.utils.PropertiesManager;
-
+ 
 public class DBManager {
 
 	public final String CAR_MAKES_QUERY_ALL = "SELECT MAKE FROM CARS"; //
