ea1a75aa4af6f05f106d8156d81216519197656a
fuzzyanalysis <sh4ngithub>
2015-10-11 16:17:56 +1100

    initial commit

---
+++ accadacca-webservices/src/main/java/com/fuzzyanalysis/accadacca/webservices/RESTApplication.java
@@ -0,0 +1,25 @@
+package com.fuzzyanalysis.accadacca.webservices;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RESTApplication extends Application {
+
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> empty = new HashSet<Class<?>>();
+	public RESTApplication(){
+	     singletons.add(new Resources());
+	}
+	@Override
+	public Set<Class<?>> getClasses() {
+	     return empty;
+	}
+	@Override
+	public Set<Object> getSingletons() {
+	     return singletons;
+	}
+}
---
+++ accadacca-webservices/src/main/java/com/fuzzyanalysis/accadacca/webservices/Resources.java
@@ -0,0 +1,41 @@
+package com.fuzzyanalysis.accadacca.webservices;
+
+import java.io.File;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/rest")
+public class Resources {
+	
+	@Path("/test")
+	@GET()
+	@Produces("text/html")
+	public String test(){
+		return "<html><body>The web services work.</body></html>";
+	}
+	
+	@Path("/geolocation/{ip}")
+	@GET()
+	@Produces("application/json")
+	public String getGeolocation(@PathParam("ip") final String ip) {
+	    
+		// get Server location		
+		String currentDir = new File(".").getAbsolutePath();
+		System.out.println(currentDir);
+				
+		try {
+
+			ExternalWebClient wc = new ExternalWebClient("", "");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		
+		return "";
+		
+	}
+	
+}
